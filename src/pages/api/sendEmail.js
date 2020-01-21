3a1280b43ab7b2a3794d4088967536fa7ed16757
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2020-01-20 22:38:48 -0500

    email formatting/template added

--- src/pages/api/sendEmail.js
+++ src/pages/api/sendEmail.js
@@ -1,18 +1,20 @@
-const sgMail = require('@sendgrid/mail')
+const sgMail = require("@sendgrid/mail");
 // const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 
 export default async function(req, res) {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  const { firstName, lastName, email, contactReason, message } = req.body
+  const { firstName, lastName, email, contactReason, message } = req.body;
 
   const note = {
+    template_id: "d-c6535418cfb34d7798280f1642247430",
     personalizations: [
       {
         to: {
-          email: "hello@creativelogic.dev"
+          email: "hello@creativelogic.dev",
+          name: "Laurie Bernadel"
         },
-        subject: `New ${contactReason} message on creativelogic.dev!`
+        subject: "New message!"
       }
     ],
     from: {
@@ -23,23 +25,22 @@ export default async function(req, res) {
       email: email,
       name: `${firstName} ${lastName}`
     },
-    content: [
-      {
-        type: "text/html",
-        value: `A message from ${firstName} ${lastName}:\n
-        ${message}`
-      }
-    ]
-  }
+    custom_args: `${contactReason}`,
+    content: {
+      type: "text/html",
+      value: `${message}`
+    }
+  };
 
   try {
-    await sgMail.send(note)
-    res.status(202).send("Your note was sent successfully! Thank you!")
-    
+    await sgMail.send(note);
+    res.status(202).send("Your note was sent successfully! Thank you!");
   } catch (error) {
-    console.log("ERROR -------->", error)
+    console.log("ERROR -------->", error);
     if (error) {
-      res.send("Sorry, your note didn't go through. Please try again or email me directly")
+      res.send(
+        "Sorry, your note didn't go through. Please try again or email me directly"
+      );
     }
   }
 }
