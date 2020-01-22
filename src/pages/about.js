13b27321cc0d70ee315ad29d059d3b2e668b4fca
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2020-01-22 01:53:29 -0500

    new content added

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
+import { Row, Col, Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
-import Layout from '../components/layout'
+import Layout from "../components/layout";
 
 const About = () => (
   <Layout>
@@ -9,8 +10,37 @@ const About = () => (
       <title>About | Creative Logic</title>
     </Head>
 
-      <h1 className="mt-5">About</h1>
-      <h6>This page is currently under construction. It'll be updated soon!</h6>
+    <h1 className="mt-5">About</h1>
+    <h4 style={{ fontFamily: '"Sedgwick Ave", cursive', color: "#88527f" }} className="mb-3">
+        ...now I'll tell you all about [array interval of stuff]. How much time do you have?
+      </h4>
+
+    <Row className="mb-5">
+      <Col lg={4}>
+        <Card>
+          <Card.Img variant="top" src="https://placedog.net/500?id=4" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Cras justo odio</ListGroupItem>
+            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+      Other stuff
+      </Col>
+    </Row>
   </Layout>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import { Jumbotron, Button, Container } from 'react-bootstrap'
+import Link from "next/link";
+import { Jumbotron, Button, Container } from "react-bootstrap";
 
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
@@ -12,16 +13,35 @@ const Home = () => (
       <title>Home | Creative Logic</title>
     </Head>
 
-    <Jumbotron className="mt-5" fluid={true} style={{backgroundColor: "rgba(159, 135, 175, 0.7)"}}>
-      <Container style={{paddingRight: "3rem", paddingLeft: "3rem"}}>
-      <h1>Hello, I'm Laurie!</h1>
-      <br />
-      <h5>I enhance web experiences through the magic of code.</h5>
-      <br />
-      <Button variant="info" href="/creations">Turn <em>ideas</em> into <em>reality</em></Button>
+    <Jumbotron
+      className="mt-5"
+      fluid={true}
+      style={{ backgroundColor: "rgba(159, 135, 175, 0.7)" }}
+    >
+      <Container style={{ paddingRight: "3rem", paddingLeft: "3rem" }}>
+        <h1>Hello, I'm Laurie!</h1>
+        <br />
+        <h5>I build web experiences through the magic of code.</h5>
+        <br />
+
+        <Link href="/creations" passHref>
+          <Button
+            variant="info"
+            size="lg"
+            aria-label="link to creations page"
+            className="my-auto"
+          >
+            Turn <em>ideas</em> into <em>reality</em>
+          </Button>
+        </Link>
+
+        <h6 className="mt-3">
+          <Link href="/about" passHref>
+            <a aria-label="About" id="whoLink">More about me.</a>
+          </Link>
+        </h6>
       </Container>
     </Jumbotron>
-
   </Layout>
 );
 
